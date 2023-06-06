f7e433d5a8ad97f2c6a8b2b628bee1d9138181ee
WeslleyGabriel <gabrielweslley880@gmail.com>
2023-06-06 03:07:13 -0300

    Projeto em 80%

---
+++ ReproduçãoVD.js
@@ -0,0 +1,5 @@
+function reproduzirVideo() {
+    const videoElement = document.getElementById('kay');
+    videoElement.src = "https://www.youtube.com/embed/4B4LMfD6aeE?list=RDEMKbikyTBSkfEgKgYNNRSg7Q";
+  }
+  
